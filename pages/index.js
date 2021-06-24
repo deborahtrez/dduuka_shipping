3cb06ecd23ce9c6f64def577ecaa92b6f3f281a3
deborahtrez <deborahtrez12@gmail.com>
2021-06-24 13:56:31 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page, Button } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Welcome to Dduuka Shipping</Heading>
+  </Page>
+);
+
+export default Index;
